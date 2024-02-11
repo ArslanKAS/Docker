0a8f78c1b5cea30b48ba4d4d0122083257963781
ArslanKAS <arslanchaos@gmail.com>
2024-02-11 20:47:05 +0500

    Changes

--- App/main.py
+++ App/main.py
@@ -1,21 +1,42 @@
-import time 
-import socket
-from sklearn.datasets import load_iris
+# 1. Import Libraries
+import json
+import uvicorn
+from dataclasses import dataclass, field
+from fastapi import FastAPI, HTTPException, Response
 
-data = load_iris()
-print("Hi there")
+# 2. Create App Instance
+app = FastAPI()
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9999))
+# 3. Define Data Class
+@dataclass
+class Channel:
+    id: str
+    name: str
+    tags: list[str] = field(default_factory=list)
+    description: str = ""
 
-server.listen()
+# 4. Initiate a Channel Dictionary that contains details of YouTube channels
+channels: dict[str, Channel] = {}
 
-while True:
-    client, addr = server.accept()
-    print("Connection from", addr)
-    client.send("You're connected!\n".encode())
-    client.send("Hello Boys!\n".encode())
-    # client.send(f"{data['data'][:,0]}\n".encode())
-    time.sleep(2)
-    client.send("You're being disconnected!\n".encode())
-    client.close()
+with open("channels.json", encoding="utf8") as file:
+    channels_raw = json.load(file)
+    for channel_raw in channels_raw:
+        channel = Channel(**channel_raw)
+        channels[channel.id] = channel
+
+# 5. Define a GET request function for Homepage
+@app.get("/")
+def read_root() -> Response:
+    return Response("The server is running.")
+
+# 6. Define a GET request function for Channels
+@app.get("/channels/{channel_id}", response_model=Channel)
+def read_item(channel_id: str) -> Channel:
+    if channel_id not in channels:
+        raise HTTPException(status_code=404, detail="Channel not found")
+    return channels[channel_id]
+
+# Run the App through Uvicorn
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+# uvicorn main:app --reload
